0583e5cb2daaeb55c4c8948bba8bb6c765919cc3
Roman Dolgiy <roman@bravetoasterstudio.com>
2010-10-13 23:15:04 +0300

    Django 1.2 compability fixes, app more reusable
    * Add csrf_token tag
    * Fixes in forms.py
    * Make app more reusable

--- csvimporter/forms.py
+++ csvimporter/forms.py
@@ -1,46 +1,62 @@
-import csv, re
+from copy import copy
 
 from django import forms
 from django.contrib.contenttypes.models import ContentType
-from django.core.files.base import ContentFile
 from django.conf import settings
+from django.db import models
+from django.db.models.fields import FieldDoesNotExist
+from django.db import IntegrityError
+from django.contrib import messages
+from django.utils.translation import ugettext as _
 
 from csvimporter.models import CSV
+from csvimporter.utils import create_csv_reader
 
 class CSVForm(forms.ModelForm):
+    def __init__(self, app=None, *args, **kwargs):
+        self.app = app
+        super(CSVForm, self).__init__(*args, **kwargs)
+        content_types = ContentType.objects.all()
+        exclude_types = getattr(settings, 'CSVIMPORTER_EXCLUDE', [])
+        # TODO: this could be so much nicer.
+        for t in exclude_types:
+            if '.' in t:
+                content_types = content_types.exclude(app_label__iexact=t.split('.')[0], model__iexact=t.split('.')[1].lower())
+            else:
+                content_types = content_types.exclude(app_label__iexact=t)
+        self.fields['content_type'] = forms.ModelChoiceField(queryset=content_types)
+        
+        if self.app:
+            self.fields['content_type'].initial = content_types.get(app_label=self.app)
+            self.fields['content_type'].widget = forms.widgets.HiddenInput()
     class Meta:
         model = CSV
-    exclude_types = getattr(settings, 'CSVIMPORTER_EXCLUDE', [])
-    # TODO: this could be so much nicer.
-    content_types = ContentType.objects.all()
-    for t in exclude_types:
-        if '.' in t:
-            content_types = content_types.exclude(app_label__iexact=t.split('.')[0], model__iexact=t.split('.')[1].lower())
-        else:
-            content_types = content_types.exclude(app_label__iexact=t)
-    content_type = forms.ModelChoiceField(queryset=content_types)
     
 key_to_field_map = getattr(settings, 'CSVIMPORTER_KEY_TO_FIELD_MAP', lambda k: k.replace(' ','_').lower())
 class CSVAssociateForm(forms.Form):
     def __init__(self, instance, *args, **kwargs):
         self.instance = instance
-        # BUGFIX: this removes trailing spaces in fieldnames.
-        instance.csv_file.file[0] = re.sub('\s+,', ',', instance.csv_file.file[0])
-        self.reader = csv.DictReader(instance.csv_file)
-        self.reader.next()
+        self.reader = create_csv_reader(instance.csv_file.file)
+#        self.reader.next()
         self.klass = self.instance.content_type.model_class()
         choices = [(None,'---- (None)')] + [(f.name, f.name) for f in self.klass._meta.fields]
-        # self.base_fields gets copied over to create self.fields in __init__
-        self.base_fields = {}
-        for field_name in self.reader.fieldnames:
-            self.base_fields[field_name] = forms.ChoiceField(choices=choices, required=False)
-            if key_to_field_map(field_name) in [f.name for f in self.klass._meta.fields]:
-                self.base_fields[field_name].initial = key_to_field_map(field_name)
         super(CSVAssociateForm, self).__init__(*args, **kwargs)
+        for field_name in self.reader.fieldnames:
+            self.fields[field_name] = forms.ChoiceField(choices=choices, required=False)
+            mapped_field_name = key_to_field_map(field_name)
+            if mapped_field_name in [f.name for f in self.klass._meta.fields]:
+                self.fields[field_name].initial = mapped_field_name
+            else:
+                _choices = copy(choices)
+                _choices.append((mapped_field_name, mapped_field_name))
+                self.fields[field_name] = forms.ChoiceField(choices=_choices, required=False)
+                self.fields[field_name].initial = mapped_field_name
         
     def save(self, request):
         # these are out here because we only need to retreive them from settings the once.
         transforms = getattr(settings, 'CSVIMPORTER_DATA_TRANSFORMS', {})
+        dups = 0
+        ok = 0
         for row in self.reader:
             data = {}
             for field_name in self.reader.fieldnames:
@@ -49,6 +65,22 @@ class CSVAssociateForm(forms.Form):
             data = transforms.get(transform_key, lambda r, d: d)(request, data)
             new_obj = self.klass()
             for key in data.keys():
+                try:
+                    field = new_obj._meta.get_field(key)
+                except FieldDoesNotExist:
+                    continue
+                if type(field) in [models.IntegerField, models.FloatField]:
+                    if not data[key]:
+                        data[key] = None
                 setattr(new_obj, key, data[key])
-            new_obj.save()
+            try:
+                new_obj.save()
+                ok += 1
+            except IntegrityError:
+                dups += 1
+        if ok:
+            messages.info(request, _("Successfully imported %s records.") % ok)
+        if dups:
+            messages.warning(request,
+                _("%s records skipped because of duplication.") % dups)
         self.instance.delete()
--- csvimporter/views.py
+++ csvimporter/views.py
@@ -8,43 +8,50 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.views.generic.list_detail import object_list, object_detail
+from django.contrib import messages
 
 from csvimporter.models import CSV
 from csvimporter.forms import CSVForm, CSVAssociateForm
 
-@staff_member_required
-def csv_list(request):
-    return object_list(request, queryset=CSV.objects.all(), template_name='csv_list.html', template_object_name='csv')
+CSVIMPORTER_REDIRECT_URL = getattr(
+    settings, "CSVIMPORTER_REDIRECT_URL", "csv-list")
+CSVIMPORTER_DEFAULT_APP = getattr(settings, "CSVIMPORTER_DEFAULT_APP", None)
 
 @staff_member_required
-def associate(request, object_id):
+def csv_list(request, **kwargs):
+    return object_list(request, queryset=CSV.objects.all(), template_name='csvimporter/csv_list.html', template_object_name='csv')
+
+@staff_member_required
+def associate(
+    request, object_id, redirect_url=CSVIMPORTER_REDIRECT_URL, **kwargs
+    ):
     instance = get_object_or_404(CSV, pk=object_id)
     if request.method == 'POST':
         form = CSVAssociateForm(instance, request.POST)
         if form.is_valid():
             form.save(request)
             request.user.message_set.create(message='CSV imported.')
-            return HttpResponseRedirect(reverse('csv-list'))
+            return HttpResponseRedirect(reverse(redirect_url))
     else:
+        messages.info(request, 'Uploaded CSV. Please associate fields below.')
         form = CSVAssociateForm(instance)
     return object_detail(request,
         queryset=CSV.objects.all(),
         object_id=object_id,
-        template_name='csv_detail.html',
+        template_name='csvimporter/csv_detail.html',
         template_object_name='csv',
         extra_context={
             'form':form,
         })
     
 @staff_member_required
-def new(request):
+def new(request, app=CSVIMPORTER_DEFAULT_APP, **kwargs):
     if request.method == 'POST':
-        form = CSVForm(request.POST, request.FILES)
+        form = CSVForm(app, request.POST, request.FILES)
         if form.is_valid():
             instance = form.save()
-            request.user.message_set.create(message='Uploaded CSV. Please associate fields below.')
             return HttpResponseRedirect(reverse('associate-csv',args=[instance.id]))
     else:
-        form = CSVForm()
-    return render_to_response('new.html',
+        form = CSVForm(app)
+    return render_to_response('csvimporter/new.html',
         {'form':form}, context_instance=RequestContext(request))
